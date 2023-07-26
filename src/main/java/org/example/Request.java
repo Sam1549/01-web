31260b851c4285a531527914204f2f48271529d0
Sam1549 <Sam1549@yandex.ru>
2023-07-27 00:40:24 +0400

    query

--- src/main/java/org/example/Request.java
+++ src/main/java/org/example/Request.java
@@ -1,30 +1,52 @@
 package org.example;
 
-import java.io.BufferedReader;
+import org.apache.http.NameValuePair;
+
+import java.util.List;
 
 
 public class Request {
     private final String method;
     private final String path;
-    private final BufferedReader body;
+    private final List<String> headers;
+    private final String body;
+    private List<NameValuePair> QueryParams;
 
-    public Request(String method, String path, BufferedReader body){
+    public Request(String method, String path, List<String> headers, String body) {
         this.method = method;
         this.path = path;
+        this.headers = headers;
         this.body = body;
     }
-    public String getMethod(){
+
+    public void setQueryParams(List<NameValuePair> nameValuePairs) {
+        this.QueryParams = nameValuePairs;
+    }
+
+    public NameValuePair getQueryParam(String name){
+        return this.QueryParams.stream().filter(s -> s.getName().equals(name)).findFirst().get();
+    }
+    public List<NameValuePair> getQueryParams(){
+        return this.QueryParams;
+    }
+    public String getMethod() {
         return method;
     }
-    public String getPath(){
+
+    public String getPath() {
         return path;
     }
+
+
+
     @Override
-    public String toString(){
+    public String toString() {
         return "Request{" +
                 "method='" + method + '\'' +
                 ", path='" + path + '\'' +
+                ", headers=" + headers +
                 ", body='" + body + '\'' +
+                ", QueryParams=" + QueryParams +
                 '}';
     }
 }
--- src/main/java/org/example/Server.java
+++ src/main/java/org/example/Server.java
@@ -1,23 +1,50 @@
 package org.example;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import org.apache.http.client.utils.URLEncodedUtils;
+
+import java.io.*;
 import java.net.ServerSocket;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
+    public static final String GET = "GET";
+    public static final String POST = "POST";
+    final static List<String> allowedMethods = List.of(GET, POST);
     private final static Map<String, Map<String, Handler>> handlers = new ConcurrentHashMap<>();
 
     public static void main(String[] args) {
+        addHandler("GET", "/vinni.png", (request, bufferedOutputStream) -> {
+            final Path filePath = Path.of(".", "public", request.getPath());
+            final String mimeType;
+            try {
+                mimeType = Files.probeContentType(filePath);
+                final long length = Files.size(filePath);
+                bufferedOutputStream.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                Files.copy(filePath, bufferedOutputStream);
+                bufferedOutputStream.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
         for (String validPath : validPaths) {
             addHandler("GET", validPath, (request, responseStream) -> {
                 try {
@@ -36,21 +63,6 @@ public class Server {
             }
         });
 
-        addHandler("GET", "/index2.html", (request, responseStream) -> {
-            try {
-                sendResponse(request, responseStream);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-
-        addHandler("GET", "/vinni.png", (request, responseStream) -> {
-            try {
-                sendResponse(request, responseStream);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
 
         new Server();
     }
@@ -67,7 +79,7 @@ public class Server {
                 var socket = serverSocket.accept();
                 var thread = new Thread(() -> {
                     try {
-                        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        BufferedInputStream in = new BufferedInputStream(socket.getInputStream());
                         BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream());
                         logicResponse(in, out);
                     } catch (IOException e) {
@@ -81,18 +93,95 @@ public class Server {
         }
     }
 
-    private void logicResponse(BufferedReader in, BufferedOutputStream out) throws IOException {
+    private void logicResponse(BufferedInputStream in, BufferedOutputStream out) throws IOException {
         Request request = getRequest(in, out);
         Handler handler = handlers.get(request.getMethod()).get(request.getPath());
         handler.handle(request, out);
+        System.out.println(request);
+        System.out.println(request.getQueryParam("value"));
     }
 
-    private static Request getRequest(BufferedReader in, BufferedOutputStream out) throws IOException {
-        final var requestLine = in.readLine();
-        final var parts = requestLine.split(" ");
-        checkBadRequest(out, parts);
-        Request request = new Request(parts[0], parts[1], in);
-        checkNotFound(out, request);
+    private static Request getRequest(BufferedInputStream in, BufferedOutputStream out) throws IOException {
+
+        // лимит на request line + заголовки
+        final var limit = 4096;
+
+        in.mark(limit);
+        final var buffer = new byte[limit];
+        final var read = in.read(buffer);
+
+        // ищем request line
+        final var requestLineDelimiter = new byte[]{'\r', '\n'};
+        final var requestLineEnd = indexOf(buffer, requestLineDelimiter, 0, read);
+        if (requestLineEnd == -1) {
+            badRequest(out);
+//                continue;
+        }
+
+        // читаем request line
+        final var requestLine = new String(Arrays.copyOf(buffer, requestLineEnd)).split(" ");
+        if (requestLine.length != 3) {
+            badRequest(out);
+//                continue;
+        }
+
+        final var method = requestLine[0];
+        if (!allowedMethods.contains(method)) {
+            badRequest(out);
+//                continue;
+        }
+        System.out.println(method);
+
+        var path = requestLine[1];
+        if (!path.startsWith("/")) {
+            badRequest(out);
+//                continue;
+        }
+
+        if(path.contains("?")) path = path.substring(0,path.indexOf("?"));
+        System.out.println(path);
+
+        // ищем заголовки
+        final var headersDelimiter = new byte[]{'\r', '\n', '\r', '\n'};
+        final var headersStart = requestLineEnd + requestLineDelimiter.length;
+        final var headersEnd = indexOf(buffer, headersDelimiter, headersStart, read);
+        if (headersEnd == -1) {
+            badRequest(out);
+//                continue;
+        }
+
+        // отматываем на начало буфера
+        in.reset();
+        // пропускаем requestLine
+        in.skip(headersStart);
+
+        final byte[] headersBytes = in.readNBytes(headersEnd - headersStart);
+        final var headers = Arrays.asList(new String(headersBytes).split("\r\n"));
+        System.out.println(headers);
+
+        // для GET тела нет
+        String body = "";
+        if (!method.equals(GET)) {
+            in.skip(headersDelimiter.length);
+            // вычитываем Content-Length, чтобы прочитать body
+            final var contentLength = extractHeader(headers);
+            if (contentLength.isPresent()) {
+                final var length = Integer.parseInt(contentLength.get());
+                final var bodyBytes = in.readNBytes(length);
+
+                body = new String(bodyBytes);
+                System.out.println(body);
+            }
+        }
+        Request request = new Request(method, path, headers, body);
+        URI url = null;
+        try {
+            url = new URI(requestLine[1]);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        assert url != null;
+        request.setQueryParams(URLEncodedUtils.parse(url, StandardCharsets.UTF_8));
         return request;
     }
 
@@ -115,7 +204,7 @@ public class Server {
             out.write(content);
         }
 
-        final Long length = Files.size(filePath);
+        final long length = Files.size(filePath);
         out.write((
                 "HTTP/1.1 200 OK\r\n" +
                         "Content-Type: " + mimeType + "\r\n" +
@@ -128,38 +217,44 @@ public class Server {
     }
 
 
-    private static void checkNotFound(BufferedOutputStream out, Request request) throws IOException {
-        if (!handlers.get(request.getMethod()).containsKey(request.getPath())) {
-            out.write((
-                    "HTTP/1.1 404 Not Found\r\n" +
-                            "Content-Length: 0\r\n" +
-                            "Connection: close\r\n" +
-                            "\r\n"
-            ).getBytes());
-            out.flush();
-
-        }
-
-    }
-
-    private static void checkBadRequest(BufferedOutputStream out, String[] parts) throws IOException {
-        if (parts.length != 3) {
-            out.write((
-                    "HTTP/1.1 400 Bad Request\r\n" +
-                            "Content-Length: 0\r\n" +
-                            "Connection: close\r\n" +
-                            "\r\n"
-            ).getBytes());
-            out.flush();
-        }
-    }
-
     public static void addHandler(String method, String path, Handler handler) {
         if (handlers.containsKey(method)) {
             handlers.get(method).put(path, handler);
         } else {
             handlers.put(method, new ConcurrentHashMap<>(Map.of(path, handler)));
         }
+    }
+
+    private static Optional<String> extractHeader(List<String> headers) {
+        return headers.stream()
+                .filter(o -> o.startsWith("Content-Length"))
+                .map(o -> o.substring(o.indexOf(" ")))
+                .map(String::trim)
+                .findFirst();
+    }
+
+    private static void badRequest(BufferedOutputStream out) throws IOException {
+        out.write((
+                "HTTP/1.1 400 Bad Request\r\n" +
+                        "Content-Length: 0\r\n" +
+                        "Connection: close\r\n" +
+                        "\r\n"
+        ).getBytes());
+        out.flush();
+    }
+
+    // from Google guava with modifications
+    private static int indexOf(byte[] array, byte[] target, int start, int max) {
+        outer:
+        for (int i = start; i < max - target.length + 1; i++) {
+            for (int j = 0; j < target.length; j++) {
+                if (array[i + j] != target[j]) {
+                    continue outer;
+                }
+            }
+            return i;
+        }
+        return -1;
     }
 
 
